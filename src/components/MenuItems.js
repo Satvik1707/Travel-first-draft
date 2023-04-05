c73fdc918e5eed062cb6ec3ed2b924046e347d8a
Satvik Madaan <satvik0125@gmail.com>
2023-04-05 21:19:08 +0530

    menu changes

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -13,16 +13,22 @@ export const MenuItems = [
     icon: "fa-solid fa-briefcase",
   },
   {
-    title: "Contact Us",
-    url: "/contact-us",
-    cName: "nav-links",
-    icon: "fa-solid fa-address-book",
-  },
-  {
     title: "Hospital",
     url: "/hospital-packages",
     cName: "nav-links",
     icon: "fa fa-plus-square",
+  },
+  {
+    title: "About",
+    url: "/about-us",
+    cName: "nav-links",
+    icon: "fa-solid fa-circle-info",
+  },
+  {
+    title: "Contact Us",
+    url: "/contact-us",
+    cName: "nav-links",
+    icon: "fa-solid fa-address-book",
   },
   // {
     //   title: "Weather",
@@ -30,10 +36,4 @@ export const MenuItems = [
     //   cName: "nav-links",
     //   icon: "fa-solid fa-cloud",
     // },
-    {
-      title: "About",
-      url: "/about-us",
-      cName: "nav-links",
-      icon: "fa-solid fa-circle-info",
-    },
   ];
